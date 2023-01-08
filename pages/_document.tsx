e9c999a333023f0b17d46098faca082bf03e94a2
JunHee <macjjuni@gmail.com>
2023-01-08 23:06:57 +0900

    first commit

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+// pages/_document.tsx
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+
+export default class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html lang="ko">
+        <Head>
+          <meta name="description" content="꾸생의 포트폴리오, Kkusaeng, macjjuni" />
+        </Head>
+        <body>
+          <Main />
+        </body>
+        <NextScript />
+      </Html>
+    )
+  }
+}
---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <>
+      <h2>About page</h2>
+      <p>123</p>
+    </>
+  )
+}
+
+export default About
