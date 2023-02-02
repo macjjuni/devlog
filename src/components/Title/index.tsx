2e898d4e257af41bb31fec1c403011e31496cf44
junheelee <jhlee@infracube.co.kr>
2023-02-02 09:42:14 +0900

    fix(src/components/Title): 리팩토링 및 환경변수 유무 검사로직 추가

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,22 +1,31 @@
 import Head from 'next/head'
 import { useEffect, useLayoutEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import { router } from '../../router'
+import { pages } from '../../router'
 
 const useMultiEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect
-const mainTitle = process.env.NEXT_PUBLIC_TITLE as string
+const mainTitle: string | undefined = process.env.NEXT_PUBLIC_TITLE
 
 const Titles = () => {
   const [htmlTitle, setTitle] = useState(mainTitle)
-
   const { route } = useRouter()
+
   useMultiEffect(() => {
-    const page = router.find((rou) => rou.path === route)
-    if (page) {
-      setTitle(`${mainTitle} - ${page.title}`)
+    const currentPage = pages.find((page) => page.path === route)
+    // 환경변수에서 메인 타이틀 유무 체크
+    if (mainTitle) {
+      if (currentPage) {
+        setTitle(`${mainTitle} - ${currentPage.title}`)
+      } else {
+        setTitle(`${mainTitle} - 404 Not Found`)
+      }
       return
     }
-    setTitle(`${mainTitle} - 404 Not Found`)
+    // 메인 타이틀 없을 경우 에러메세지 출력
+    if (currentPage) {
+      setTitle(`No Title - ${currentPage.title}`)
+      console.error('Not found main title in environment variable')
+    }
   }, [route])
 
   return (
