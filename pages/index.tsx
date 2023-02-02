e74cd2b669a89b8b2fadba0ac81b7eaed0008133
junheelee <jhlee@infracube.co.kr>
2023-02-02 14:38:38 +0900

    feat(component/voxel): three.js 라이브러리 추가 및 작업중

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
 import type { NextPage } from 'next'
+import dynamic from 'next/dynamic'
 import { IndexLayout } from '../styles'
 
-// import dynamic from 'next/dynamic'
-
-// const Voxel = dynamic(() => import('../src/components/Voxel'), {
-//   ssr: false,
-// })
+const Voxel = dynamic(() => import('../src/components/Voxel'), {
+  ssr: false,
+})
 
 const Home: NextPage = () => {
   return (
     <IndexLayout>
       <h2>Index Page</h2>
-      {/* <Voxel /> */}
+      <Voxel />
     </IndexLayout>
   )
 }
--- src/components/Voxel/index.tsx
+++ src/components/Voxel/index.tsx
@@ -1,9 +1,18 @@
-// import {  useEffect,  } from 'react'
-// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-// import * as THREE from 'three'
+import { useEffect, useRef } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 const Voxel = () => {
-  return <>Voxel</>
+  return (
+    <Canvas style={{ height: '500px' }}>
+      <OrbitControls />
+      <mesh>
+        <ambientLight intensity={0.5} />
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial attach="material" color={0xa3b18a} />
+      </mesh>
+    </Canvas>
+  )
 }
 
 export default Voxel
