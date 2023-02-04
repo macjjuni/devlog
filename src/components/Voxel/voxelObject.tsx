743453f170fb48d338c4a28e1956abf8df2f932c
JunHee <macjjuni@gmail.com>
2023-02-05 04:22:37 +0900

    style: voxel 스타일 수정

---
+++ src/components/Voxel/voxelObject.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { useLoader, useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
+
+const options = {
+  scale: 0.05,
+  position: [0, 1.5, 1],
+}
+
+const VoxelObject = ({ url }: { url: string }) => {
+  const { scene, animations } = useLoader(GLTFLoader, url)
+  const [mixer] = useState<THREE.AnimationMixer>(() => new THREE.AnimationMixer(scene))
+
+  useFrame((state, delta) => mixer.update(delta))
+  useEffect(() => {
+    mixer.clipAction(animations[0]).play()
+    return () => animations.forEach((clip) => mixer.uncacheClip(clip))
+  }, [])
+
+  return <primitive object={scene} scale={options.scale} position={options.position} />
+}
+
+export default VoxelObject
