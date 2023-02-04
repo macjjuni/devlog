007fc38bf258f1258972730ff44ab423263e11dd
JunHee <macjjuni@gmail.com>
2023-02-05 04:23:22 +0900

    feat(pages/index,about): 페이지 컨텐츠 작성 중

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,25 @@
-import { AboutStyled } from '../styles/about'
+import styled from 'styled-components'
+
+export const AboutStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 20px;
+  margin: 0;
+  & > p {
+    font-family: 'NanumBarunGothic';
+    ${({ theme }) => theme.fontStyle.pc.heading_sm}
+  }
+`
 
 const About = () => {
   return (
     <AboutStyled>
-      <h2>About page</h2>
+      <p>1년 차 프론트엔드 개발자 이준희입니다.</p>
+      <p>꾸준함이 생명이라는 인생 모토를 가지고 있으며, 개발을 통해 이것저것 만드는 걸 좋아합니다.</p>
+      <p>새로 알게 된 개발 내용을 기록하는 블로그를 운영하면서 지금까지 200개 이상 포스팅을 작성했고 현재 진행 중입니다.</p>
+      <p>최근 DX(Developer Experience)와 웹에서 앱과 같은 사용자 경험을 구현하는 데 관심이 있습니다.</p>
     </AboutStyled>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,37 @@
-import type { NextPage } from 'next'
-import dynamic from 'next/dynamic'
-import { IndexLayout } from '../styles'
+import styled from 'styled-components'
+import Mail from '../src/components/Mail'
 
-const Voxel = dynamic(() => import('../src/components/Voxel'), {
-  ssr: false,
-})
+const IndexStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 100%;
+  & > p {
+    font-family: 'NanumBarunGothic';
+    ${({ theme }) => theme.fontStyle.pc.heading_sm}
+  }
+`
 
-const Home: NextPage = () => {
+const Spacer = styled.div`
+  height: 40px;
+`
+
+const Home = () => {
   return (
-    <IndexLayout>
-      <h2>Index Page</h2>
-      <Voxel />
-    </IndexLayout>
+    <IndexStyled>
+      <p>
+        안녕하세요? 🙋🏻‍♂️👋
+        <br />
+        이것저것 만들기 좋아하는 개발자입니다!
+      </p>
+      <Spacer />
+      <p>
+        저에게 관심이 있으시거나, 궁금한 점이 있다면, <br />
+        아래 이메일로 언제든지 연락주세요.
+      </p>
+      <Mail />
+    </IndexStyled>
   )
 }
 
