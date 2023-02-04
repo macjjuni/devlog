edddb07f8ad9a854c5973f94f1d39010a68bca82
JunHee <macjjuni@gmail.com>
2023-02-05 04:23:02 +0900

    feat(comp/Mail): 메인페이지 Mail 컴포넌트 개발

---
+++ src/components/Mail/index.tsx
@@ -0,0 +1,32 @@
+import { useCallback } from 'react'
+import { MailWrap, LottieCustom, Spacer, MailText } from './style'
+import Lotties from '../Lottie'
+import errorLottie from '../../assets/lottie/mail-lottie.json'
+
+// Lottie Option
+const defaultOption = {
+  loop: true,
+  play: true,
+  style: {
+    width: '50px',
+    height: '50px',
+  },
+}
+
+const Mail = () => {
+  const copyHandle = useCallback(() => {
+    console.log('Copied!')
+  }, [])
+
+  return (
+    <MailWrap onClick={copyHandle}>
+      <LottieCustom>
+        <Lotties defaultOption={defaultOption} animationData={errorLottie} />
+      </LottieCustom>
+      <Spacer />
+      <MailText className="mail-text">macjjuni@gmail.com</MailText>
+    </MailWrap>
+  )
+}
+
+export default Mail
---
+++ src/components/Mail/style.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const MailWrap = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  margin-top: 50px;
+  cursor: pointer;
+
+  &:hover {
+    & > .mail-text::after {
+      width: 100%;
+    }
+  }
+`
+
+export const Spacer = styled.div`
+  width: 10px;
+`
+
+export const LottieCustom = styled.div`
+  margin: 0 -10px;
+`
+
+export const MailText = styled.div`
+  position: relative;
+  ${({ theme }) => theme.fontStyle.pc.text_lg};
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 3px;
+    left: 0;
+    width: 0%;
+    height: 10px;
+    background-color: ${({ theme }) => theme.colors.primary.MAIN};
+    transition: width 0.2s ease;
+    z-index: -1;
+  }
+`
