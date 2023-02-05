7f2a0be21b4134503ec0e3ca2e1ac37a294f0c1f
JunHee <macjjuni@gmail.com>
2023-02-06 01:09:55 +0900

    fix(type error): getPosts PORT 타입 에러 수정

--- src/api/blog/getPosts.ts
+++ src/api/blog/getPosts.ts
@@ -1,7 +1,9 @@
 import axios, { AxiosResponse } from 'axios'
 import { PostTypes } from '../../type/blog'
 
+const port = process.env.PORT || 3000
+
 export const getPostList = (): Promise<AxiosResponse<PostTypes[]>> => {
-  const url = `http://localhost:${process.env.PORT}/api/blog/getPosts`
+  const url = `http://localhost:${port}/api/blog/getPosts`
   return axios.get(url)
 }
