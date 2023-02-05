50845b4c493d2fbc2508c432ec318ed4b5e5bd6d
JunHee <macjjuni@gmail.com>
2023-02-06 01:22:47 +0900

    style(page/404): 404 페이지 스타일 수정

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,27 @@
 import styled from 'styled-components'
+import Link from 'next/link'
+import Text from '../src/components/Text'
 import Lotties from '../src/components/Lotties'
 import errorLottie from '../src/assets/lottie/error-page-not-found.json'
 
 const ErrorLayout = styled.section`
-  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding-bottom: 100px;
+  & a {
+    border: 1px solid ${({ theme }) => theme.colors.primary.MAIN};
+    color: ${({ theme }) => theme.colors.primary.MAIN};
+    padding: 4px 15px;
+    border-radius: 4px;
+    transition: 0.5s ease;
+  }
+  & a:hover {
+    color: ${({ theme }) => theme.colors.primary.MED};
+    background-color: ${({ theme }) => theme.colors.primary.LIGHT};
+  }
 `
 // Lottie Option
 const defaultOption = {
@@ -20,6 +38,15 @@ const Error = () => {
   return (
     <ErrorLayout>
       <Lotties defaultOption={defaultOption} animationData={errorLottie} />
+      <Text type="h3" variant="heading_lg">
+        404 - Not Found
+      </Text>
+      <br />
+      <Link href="/">
+        <Text type="span" variant="text_lg">
+          Go Home
+        </Text>
+      </Link>
     </ErrorLayout>
   )
 }
