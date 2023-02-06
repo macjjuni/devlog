2eb37adf33d763b2ff392d202c053d91722594a9
junheelee <macjjuni@gmail.com>
2023-02-06 10:53:44 +0900

    test(pages/about): api 호출 테스트

--- src/api/blog/getPosts.ts
+++ src/api/blog/getPosts.ts
@@ -2,8 +2,10 @@ import axios, { AxiosResponse } from 'axios'
 import { PostTypes } from '../../type/blog'
 
 const port = process.env.PORT || 3000
-const url = process.env.NODE_ENV === 'development' ? `http://localhost:${port}/api/blog/getPosts` : `/api/blog/getPosts`
+const domain = process.env.DOMAIN || 'http://localhost'
+const host = process.env.NODE_ENV === 'development' ? `${domain}:${port}` : domain
 
 export const getPostList = (): Promise<AxiosResponse<PostTypes[]>> => {
+  const url = `${host}/api/blog/getPosts`
   return axios.get(url)
 }
