bf3dd8ed8e4f282cd994451deea00cbeec6790f8
JunHee <macjjuni@gmail.com>
2023-02-08 23:23:44 +0900

    feat(comp/workList): 컴포넌트 적용 및 임시 텍스트 추가

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,13 +1,36 @@
 import styled from 'styled-components'
 import WorkList from '../src/components/workList'
+import { getWorks } from '../src/api/work/getWorks'
+import { IWork } from '../src/components/workList/type'
 
 const WorkWrap = styled.div``
 
-const Work = () => {
+const Work = ({ workList }: { workList: IWork[] }) => {
+  console.log(workList)
   return (
     <WorkWrap>
-      <WorkList />
+      {workList.map((work) => (
+        <WorkList key={work.title} work={work} />
+      ))}
+      {workList.length === 0 && <h2>작성 중입니다 😎</h2>}
     </WorkWrap>
   )
 }
+
+export const getStaticProps = async () => {
+  try {
+    const { data } = await getWorks()
+    return {
+      props: { workList: data },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: {
+        workList: [],
+      },
+    }
+  }
+}
+
 export default Work
