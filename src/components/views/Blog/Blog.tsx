168065f48160e331eb48f9c4870ec859a391ce6d
macjjuni <macjjuni@gmail.com>
2023-02-10 22:46:43 +0900

    refactor(comp/Blog): 스타일 선언 및 구조 리팩토링

--- src/components/views/Blog/Blog.tsx
+++ src/components/views/Blog/Blog.tsx
@@ -1,14 +1,20 @@
+import Text from 'components/common/Text'
 import Article from './components/Article'
-import { BlogWrap } from './Blog.style'
+import * as B from './Blog.style'
 import { type PostTypes } from '../../../type/blog'
 
 const BlogArticle = ({ posts }: { posts: PostTypes[] }) => {
   return (
-    <BlogWrap>
-      {posts.map((post) => (
-        <Article key={post.title} post={post} />
-      ))}
-    </BlogWrap>
+    <B.Blog>
+      <Text type="h3" variant="heading_md">
+        🗒️ Recent posts
+      </Text>
+      <B.BlogWrap>
+        {posts.map((post) => (
+          <Article key={post.title} post={post} />
+        ))}
+      </B.BlogWrap>
+    </B.Blog>
   )
 }
 export default BlogArticle
--- src/components/views/Blog/components/Article.tsx
+++ src/components/views/Blog/components/Article.tsx
@@ -1,25 +1,24 @@
 import Image from 'next/image'
-import Text from 'components/common/Text'
-import { type PostTypes } from 'type/blog'
 import { MdOpenInNew } from 'react-icons/md'
-import { ArticleWrap } from '../Blog.style'
+import { type PostTypes } from 'type/blog'
+import * as B from '../Blog.style'
 
 const blogURL = process.env.NEXT_PUBLIC_BLOG_URL || 'https://juni-official.tistory.com/'
 
 const Article = ({ post }: { post: PostTypes }) => {
   return (
-    <ArticleWrap>
-      <a target="_blank" rel="noreferrer" title={post.title} href={`${blogURL}${post.link}`}>
+    <B.Article>
+      <B.BlogLink target="_blank" rel="noreferrer" title={post.title} href={`${blogURL}${post.link}`}>
         <Image src={post.thumb} alt={post.title} width={270} height={150} priority />
-        <Text type="span" variant="text_md" className="post-date">
+        <B.BlogDate type="span" variant="text_md">
           <span>{post.date}</span>
           <MdOpenInNew />
-        </Text>
-        <Text type="h3" variant="text_md" className="post-title">
+        </B.BlogDate>
+        <B.BlogTitle type="h3" variant="text_md">
           {post.title}
-        </Text>
-      </a>
-    </ArticleWrap>
+        </B.BlogTitle>
+      </B.BlogLink>
+    </B.Article>
   )
 }
 export default Article
