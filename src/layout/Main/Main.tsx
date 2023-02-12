628f4f4350afeb04c0ddebbaffe35c34ba8ed071
macjjuni <macjjuni@gmail.com>
2023-02-12 21:30:39 +0900

    feat(layout/Main): Voxel 로딩을 위해 ssr 적용

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -1,4 +1,5 @@
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
+import Voxel from 'components/views/Voxel'
 import Spinner from 'components/views/Spinner'
 import { ReactNode, useEffect, useState, useCallback } from 'react'
 import { useRouter } from 'next/router'
@@ -6,9 +7,9 @@ import { pages } from 'router'
 import { MainStyled, VexelWrap, MotionStyled } from './Main.style'
 import { MainAnimation, xWidth } from './framer-motion'
 
-const Voxel = dynamic(() => import('components/views/Voxel'), {
-  ssr: false,
-})
+// const Voxel = dynamic(() => import('components/views/Voxel'), {
+//   ssr: false,
+// })
 
 const Main = ({ children }: { children: ReactNode }) => {
   // 스피너랑 복셀 컴포넌트 렌더링
