48c08a3194f31c5c98169718108d429942c0f30f
macjjuni <macjjuni@gmail.com>
2023-02-18 22:45:55 +0900

    feat: moment 설치 및 시간 포멧 적용

--- notion/components/card/CardItem.tsx
+++ notion/components/card/CardItem.tsx
@@ -1,52 +1,35 @@
-import { motion } from 'framer-motion'
 import Image from 'next/image'
-import Link from 'next/link'
-import { CardData } from '../../types/types'
-import IconRenderer from './IconRenderer'
+import moment from 'moment'
+import { useAppSelector } from 'redux/hook'
+import * as C from './Card.style'
 import TagList from './tags/TagList'
+import type { CardData } from '../../types/types'
 
-interface CardItemsProps {
+interface ICardItem {
   data: CardData
 }
 
-const CardItem = ({ data }: CardItemsProps) => {
-  const { id, title, description, published, icon, tags, cover, lastEditedTime } = data
-
+const CardItem = ({ data }: ICardItem) => {
+  const colorMode = useAppSelector((state) => state.colorMode.theme)
+  const { id, title, published, category, tags, cover } = data
   return (
-    <motion.li
-      initial={{
-        opacity: 0,
-      }}
-      whileInView={{
-        opacity: 1,
-      }}
-      transition={{
-        duration: 1,
-      }}
-      viewport={{
-        once: true,
-      }}
-      className="list-none"
-    >
-      <article className="group">
-        <Link href={`/post/${id}`}>
-          <div className="relative pt-[64%] rounded-lg overflow-hidden mb-4">
-            <Image src={cover} alt={title} width="300" height="300" className="group-hover:scale-110 transition-all duration-300" />
-          </div>
-          <div className="flex flex-col gap-1">
-            <h2 className="text-2xl font-bold group-hover:text-blue-500">
-              <IconRenderer icon={icon} />
-              {title}
-            </h2>
-            {description ? <p className="text-gray-700">{description}</p> : null}
-            <time className="text-gray-500 font-light text-sm">{published}</time>
-          </div>
-        </Link>
-        <div className="mt-4">
-          <TagList tags={tags} />
-        </div>
-      </article>
-    </motion.li>
+    <C.CardItem colormode={colorMode}>
+      <C.CardArticle>
+        <C.CardLink href={`/post/${id}`}>
+          <C.CardImageWrap className="themeBgc">
+            <Image src={cover} alt={title} width="300" height="300" />
+          </C.CardImageWrap>
+          <C.DesWrap>
+            <C.CardTitle>{title}</C.CardTitle>
+            <C.CardSubWrap>
+              <span>{category?.name}</span>
+              <time>{moment(published).format('YYYY-MM-DD')}</time>
+            </C.CardSubWrap>
+            <TagList tags={tags} />
+          </C.DesWrap>
+        </C.CardLink>
+      </C.CardArticle>
+    </C.CardItem>
   )
 }
 
