217509ac5e39086ca27845a42e9533744b5c2158
macjjuni <macjjuni@gmail.com>
2023-02-18 22:48:09 +0900

    style: 네비게이션 메뉴 마진 수정

--- src/layout/Header/components/NavList/NavList.style.ts
+++ src/layout/Header/components/NavList/NavList.style.ts
@@ -3,11 +3,8 @@ import Link from 'next/link'
 import { dark } from 'redux/slice/colorMode'
 import type { ColorModeTypes } from 'type/theme'
 
-export const Nav = styled.nav`
-  @media ${({ theme }) => theme.device.mobile} {
-    display: none;
-  }
-`
+export const Nav = styled.nav``
+
 export const NavList = styled.ul`
   padding: 0;
   display: flex;
@@ -17,7 +14,6 @@ export const NavItem = styled.li`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 0 15px;
 `
 export const NavLink = styled(Link)<{ colormode: ColorModeTypes }>`
   position: relative;
@@ -26,11 +22,13 @@ export const NavLink = styled(Link)<{ colormode: ColorModeTypes }>`
   justify-content: center;
   align-items: center;
   height: 40px;
+  margin: 0 0 0 15px;
   padding: 5px 10px;
   font-style: italic;
   font-weight: bold;
   ${({ theme }) => theme.fontStyle.desktop.heading_sm};
   z-index: 1;
+
   &::after {
     content: '';
     position: absolute;
@@ -48,5 +46,14 @@ export const NavLink = styled(Link)<{ colormode: ColorModeTypes }>`
   }
   &.active::after {
     width: calc(100% - 20px);
+  }
+
+  @media ${({ theme }) => theme.device.mobile} {
+    margin: 0 5px;
+    ${({ theme }) => theme.fontStyle.desktop.text_md};
+    &::after {
+      height: 2px;
+      bottom: 10px;
+    }
   }
 `
