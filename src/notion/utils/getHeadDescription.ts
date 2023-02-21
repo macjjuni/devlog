1c0a0502cb2bdd48a815a2733c7f2826d69c1807
macjjuni <macjjuni@gmail.com>
2023-02-21 13:50:43 +0900

    refactor(getHeadDescription): 성능 최적화

--- src/notion/utils/getHeadDescription.ts
+++ src/notion/utils/getHeadDescription.ts
@@ -6,27 +6,26 @@ interface ICleanBlock {
   value: Block
 }
 
+const cotainsType = ['header', 'sub_header', 'text']
+
 export const getHeadDescription = (recordMap: ExtendedRecordMap) => {
   const blocks = getPageContentBlockIds(recordMap)
+
   const blockArr = blocks.map((blockId) => {
-    if (recordMap.block[blockId].value.type === 'text' && recordMap.block[blockId].value.properties) return recordMap.block[blockId]
+    if (cotainsType.includes(recordMap.block[blockId].value.type) && recordMap.block[blockId].value.properties) return recordMap.block[blockId]
   })
-  // undefined랑 Value에 값 여부 체크
-  const cleanBlockArr = blockArr.filter((block) => block && block.value) as ICleanBlock[]
+
+  // undefined 제외하고 성능 향상을 위해 블럭 5개 까지만 남기기
+  const cleanBlockArr = blockArr.filter((block): block is ICleanBlock => block !== undefined).slice(0, 5)
 
   let contentText = ''
-  // 성능 향상을 위해 최대 5개 까지만 텍스트 가져오기
-  const loopLength = blockArr.length < 5 ? blockArr.length : 5
+  // 문자열 없는 경우 빈 문자열 반환
+  if (cleanBlockArr.length === 0) return contentText
 
-  // 문자열 없는 경우 빈 문자열 리턴
-  if (cleanBlockArr !== undefined && cleanBlockArr.length === 0) return contentText
-
-  // 있는 경우 모두 합쳐서 100 글자 까지만 남기고 리턴
-  for (let i = 0; i < loopLength; i++) {
-    if (cleanBlockArr[i] === undefined) {
-      const getBlcokText = getBlockTitle(cleanBlockArr[i].value, recordMap)
-      contentText += getBlcokText
-    }
+  // 있는 경우 모두 합쳐서 100 글자 까지만 남기고 반환
+  for (let i = 0; i < cleanBlockArr.length; i++) {
+    const getBlcokText = getBlockTitle(cleanBlockArr[i].value, recordMap)
+    contentText += `${getBlcokText} `
   }
   return contentText.substring(0, 100)
 }
