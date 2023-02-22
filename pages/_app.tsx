70eb4c664f2541dcc1c5b27b5931058f4d8ca317
macjjuni <macjjuni@gmail.com>
2023-02-22 10:26:41 +0900

    refacotr: _document app.tsx 리팩토링

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,23 @@
 import type { AppProps } from 'next/app'
-import { Provider } from 'react-redux'
-import { store } from 'redux/store'
 import { ThemeProvider } from 'styled-components'
-import InitialColorMode from 'components/common/InitialColorMode'
-import GoogleAnalystics from 'components/common/GoogleAnalytics'
-import WithNProgress from 'hoc/withNProgress'
 import { theme } from 'styles/theme'
+import { GlobalStyles } from 'styles/globals'
+// import InitialColorMode from 'components/common/InitialColorMode'
+import GoogleAnalystics from 'components/common/GoogleAnalytics'
+
+import WithNProgress from 'hoc/withNProgress'
 import Layout from 'layout'
 
 import 'styles/reset.css'
 import 'styles/notion/index.scss'
+import 'styles/notion/notion.style.scss'
 import 'prismjs/themes/prism-tomorrow.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Provider store={store}>
-      {/* HTML title, meta 태그 컴포넌트 */}
+    <>
       <ThemeProvider theme={theme}>
-        {/* OS에 설정된 컬러모드 초기화 */}
-        <InitialColorMode />
+        <GlobalStyles />
         {/* 구글 애널리틱스 */}
         <GoogleAnalystics />
         <WithNProgress>
@@ -27,7 +26,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           </Layout>
         </WithNProgress>
       </ThemeProvider>
-    </Provider>
+    </>
   )
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,31 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import Favicon from 'components/common/Favicon'
-import GlobalStyle from 'components/common/GlobalStyle'
 
-export default class CustomDocument extends Document {
+export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return initialProps
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render() {
@@ -22,29 +41,5 @@ export default class CustomDocument extends Document {
         <NextScript />
       </Html>
     )
-  }
-}
-
-CustomDocument.getInitialProps = async (ctx: DocumentContext) => {
-  const sheet = new ServerStyleSheet()
-  const originalRenderPage = ctx.renderPage
-  try {
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) =>
-          sheet.collectStyles(
-            <>
-              <GlobalStyle />
-              <App {...props} />
-            </>
-          ),
-      })
-    const initialProps = await Document.getInitialProps(ctx)
-    return {
-      ...initialProps,
-      styles: [initialProps.styles, sheet.getStyleElement()],
-    }
-  } finally {
-    sheet.seal()
   }
 }
