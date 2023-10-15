0689ed436e631133cdc9a4c271607622b21215ab
macjjuni <macjjuni@gmail.com>
2023-10-15 23:19:43 +0900

    Style: 메인 페이지 반응형 적용

--- src/layouts/PageLayout/HomeLayout.tsx
+++ src/layouts/PageLayout/HomeLayout.tsx
@@ -3,11 +3,11 @@ interface IHomeLayout {
   right: React.ReactNode
 }
 
-const innerStyle = 'w-1/2 p-sm'
+const innerStyle = 'w-full md:w-1/2 p-sm'
 
 export default function HomeLayout({ left, right }: IHomeLayout) {
   return (
-    <div className="flex justify-between items-center w-full max-w-layout p-md">
+    <div className="flex flex-col md:flex-row justify-between items-center w-full max-w-layout p-md">
       <div className={innerStyle}>{left}</div>
       <div className={innerStyle}>{right}</div>
     </div>
