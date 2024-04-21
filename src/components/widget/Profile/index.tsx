68203e8b60711615ebdc28ee5ac2eb2f69f6444e
macjjuni <macjjuni@gmail.com>
2024-04-21 17:08:53 +0900

    feature. x 링크 제거 및 프로필 컴포넌트 변경

--- src/components/widget/Profile/index.tsx
+++ src/components/widget/Profile/index.tsx
@@ -1,21 +1,25 @@
 // import Image from 'next/image'
-// import common from '@/styles/common'
-import BtcPayment from '@/components/molecule/BtcPayment'
-import type { INotionInfo } from '@/@types/notion'
+import common from "@/styles/common";
+import Image from "next/image";
+import type { INotionInfo } from "@/@types/notion";
+// import BtcPayment from '@/components/molecule/BtcPayment'
 
-// const defaultStyle = `rounded-xs mb-md h-[270px] ${common.borderColor} border border-BLG300 object-cover	`
+const defaultStyle = `rounded-xs mb-md h-[270px] ${common.borderColor} border border-BLG300 object-cover	`;
 
 const Profile = ({ info }: { info: INotionInfo }) => {
+
   return (
     <div className="lg:mb-xl">
-      {/* {info.coverURL && <Image src={info?.coverURL} alt={info?.title} width={1200} height={300} className={`${defaultStyle}`} priority />} */}
-      {info.coverURL && <BtcPayment icon={info?.icon} name={info?.title} url={info?.coverURL} />}
-      {/* <h3 className="text-categoryTitle py-sm mb-sm">
-        {info?.icon} {info?.title}
-      </h3> */}
-      <p className="px-sm py-xs">{info?.description}</p>
+      {info.coverURL &&
+        <Image src={info?.coverURL} alt={info?.title} width={1200} height={300} className={`${defaultStyle}`}
+               priority />}
+      {/* {info.coverURL && <BtcPayment icon={info?.icon} name={info?.title} url={info?.coverURL} />} */}
+      {/* /!* <h3 className="text-categoryTitle py-sm mb-sm"> */}
+      {/*   {info?.icon} {info?.title} */}
+      {/* </h3> *!/ */}
+      <p className="py-xs">{info?.description}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
--- src/components/widget/SocialLink/index.tsx
+++ src/components/widget/SocialLink/index.tsx
@@ -18,14 +18,14 @@ const SocialLink = () => {
       >
         <GithubSvg size={28} /> Github
       </a>
-      <a
-        href={twitterUrl}
-        target="_blank"
-        title="트위터 새 탭에서 링크 열기"
-        className="relative flex justify-start gap-sm items-center text-body font-normal hoverUnderline"
-      >
-        <TwiiterSvg size={28} /> X(Twitter)
-      </a>
+      {/* <a */}
+      {/*   href={twitterUrl} */}
+      {/*   target="_blank" */}
+      {/*   title="트위터 새 탭에서 링크 열기" */}
+      {/*   className="relative flex justify-start gap-sm items-center text-body font-normal hoverUnderline" */}
+      {/* > */}
+      {/*   <TwiiterSvg size={28} /> X(Twitter) */}
+      {/* </a> */}
     </div>
   )
 }
