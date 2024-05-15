06bdd75fa60307595a995af83e90efd8b3d9d980
macjjuni <macjjuni@gmail.com>
2024-05-16 00:12:34 +0900

    trivial. Profile 컴포넌트 모바일 스타일 수정

--- src/component/sidebar/profile/profile.tsx
+++ src/component/sidebar/profile/profile.tsx
@@ -1,19 +1,16 @@
 import { memo } from "react";
-import Image from 'next/image';
+import Image from "next/image";
 import "./profile.scss";
 
-
 interface ProfileProps {
-  description: string
-  imageUrl: string | null
+  description: string;
+  imageUrl: string | null;
 }
 
-function Profile({description, imageUrl}: ProfileProps) {
+function Profile({ description, imageUrl }: ProfileProps) {
   return (
     <div className="profile__card">
-      {
-        imageUrl && <Image className="profile__card__image" src={imageUrl} alt="profile image" width={400} height={400} />
-      }
+      {imageUrl && <Image className="profile__card__image" src={imageUrl} alt="profile image" width={400} height={400} />}
       <div className="profile__card__content">
         <p className="profile__card__content__name">꾸생</p>
         <p className="profile__card__content__status-message">{description}</p>
