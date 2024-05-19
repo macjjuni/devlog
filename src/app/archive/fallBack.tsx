c99420c0bd73b4d8a2b6fa44833d33065ada9451
macjjuni <macjjuni@gmail.com>
2024-05-19 23:32:42 +0900

    trivial. FallBack 컴포넌트 작성

--- src/app/archive/fallBack.tsx
+++ src/app/archive/fallBack.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Skeleton from "@/component/common/skeleton/skeleton";
 
 const FallBack = () => {
-  return <div>Loading~~</div>;
+  return (
+    <>
+      <section className="archive__layout__sidebar">
+        <Skeleton height={'100px'} />
+        <Skeleton height={'68px'} />
+        <Skeleton height={'320px'} />
+      </section>
+      <section className="archive__layout__content">
+        <Skeleton height={'30px'} />
+        <Skeleton height={'648px'} />
+        <Skeleton height={'32px'} />
+      </section>
+    </>
+  )
 };
 
 export default FallBack;
--- src/component/common/skeleton/skeleton.tsx
+++ src/component/common/skeleton/skeleton.tsx
@@ -1,8 +1,24 @@
-import "./skeleton.scss";
-import { memo } from "react";
+"use client";
 
-function Skeleton() {
-  return <div className="kku-skeleton" />;
+import { CSSProperties, memo, useMemo } from "react";
+import "./skeleton.scss";
+
+function Skeleton({ height }: { height?: string }) {
+  // region [Styles]
+
+  const rootStyle = useMemo(() => {
+    const style: CSSProperties = {};
+
+    if (height) {
+      style.height = height;
+    }
+
+    return style;
+  }, [height]);
+
+  //
+
+  return <div className="kku-skeleton" style={rootStyle} />;
 }
 
 export default memo(Skeleton);
