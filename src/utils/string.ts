b871d0c4547524cfd975c06384da3b3316fdaef6
macjjuni <macjjuni@gmail.com>
2024-05-23 16:44:20 +0900

    feature. 문자열 공통 함수 작성
    - 문자열에 숫자만 추출해서 반환

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -39,3 +39,8 @@ export const dateReg = (str: string, targetStr: string, replaceTxt: string) => {
 
 /* ---------- 숫자 형식 판별 정규식 ---------- */
 export const isNumber = (str: string) => /^\d+$/.test(str);
+
+/* ---------- 숫자로 걸러주는 정규식 ---------- */
+export const extractNumbers = (str: string) => {
+  return str.replace(/\D/g, "");
+};
