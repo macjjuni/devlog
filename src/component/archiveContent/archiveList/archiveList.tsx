dc3d4ebfe7c7f9140bba2dd2d42a0b357de42d82
macjjuni <macjjuni@gmail.com>
2024-06-16 20:14:02 +0900

    feature. 최근 작성 게시물 스타일 효과 적용

--- src/component/archiveContent/archiveList/archiveList.tsx
+++ src/component/archiveContent/archiveList/archiveList.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { memo, useMemo } from "react";
+import { memo, useCallback, useMemo } from "react";
 import Link from "next/link";
 import config from "@/config/notion.config";
 import usePageSize from "@/hook/usePageSize";
 import type { NotionPageProps } from "@/@types/notion";
-import "./archiveList.scss";
 import date from "@/lib/date";
+import "./archiveList.scss";
 
-const { POSTS_PER_PAGE } = config.post;
+const { post } = config;
 
 function ArchiveList({ pages }: { pages: NotionPageProps[] }) {
   // region [Hooks]
@@ -19,7 +19,9 @@ function ArchiveList({ pages }: { pages: NotionPageProps[] }) {
 
   // region [Privates]
 
-  const pageList = useMemo(() => pages.slice(POSTS_PER_PAGE * (page - 1), POSTS_PER_PAGE * page), [page, pages]);
+  const pageList = useMemo(() => pages.slice(post.POSTS_PER_PAGE * (page - 1), post.POSTS_PER_PAGE * page), [page, pages]);
+
+  const isNew = useCallback((dateStr: string) => post.RECENT_DAY > date.nowDiff(dateStr), []);
 
   // endregion
 
@@ -27,9 +29,8 @@ function ArchiveList({ pages }: { pages: NotionPageProps[] }) {
     <ul className="archive__list">
       {pageList.map((listItem) => (
         <li key={listItem.id} className="archive__list__item">
-          <Link href={`/archive/${listItem.id}`} className="archive__list__item__link">
+          <Link href={`/archive/${listItem.id}`} className={`archive__list__item__link${isNew(listItem.published) ? " new" : ""}`} suppressHydrationWarning>
             <h3 className="archive__list__item__title">{listItem.title}</h3>
-            {/* TODO. 날짜 포멧 변환 해야함! YYYY.MM.DD */}
             <div className="archive__list__item__right">
               <p>{listItem.category?.name}</p>
               <p>{date.format(listItem.published)}</p>
@@ -37,13 +38,7 @@ function ArchiveList({ pages }: { pages: NotionPageProps[] }) {
           </Link>
         </li>
       ))}
-      {
-        pageList.length === 0 && (
-          <li className="archive__list__item-no-text">
-            검색 결과가 없습니다.
-          </li>
-        )
-      }
+      {pageList.length === 0 && <li className="archive__list__item-no-text">검색 결과가 없습니다.</li>}
     </ul>
   );
 }
