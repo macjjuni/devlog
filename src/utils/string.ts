27abb4a225b17d8fed405e09744a31a3ad85fb8f
macjjuni <macjjuni@gmail.com>
2024-10-26 23:49:19 +0900

    feature. 문자열 맨 앞 대문자로 변경해주는 함수 작성

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,9 +1,9 @@
-/* ---------- 숫자 형식 판별 정규식 ---------- */
+// 숫자 형식 판별 정규식
 import { ReactNode } from "react";
 
 export const isNumber = (str: string) => /^\d+$/.test(str);
 
-/* ---------- 숫자로 걸러주는 정규식 ---------- */
+// 숫자로 걸러주는 정규식
 export const extractNumbers = (str: string) => {
   return str.replace(/\D/g, "");
 };
@@ -14,4 +14,9 @@ export const textToId = (str: ReactNode) => {
     .toLocaleLowerCase()
     .replace(/[^\w\sㄱ-힣-]/g, " ")
     .replaceAll(" ", "-");
+};
+
+// 맨 앞 문자열만 대문자로 변경
+export const capitalizeFirstLetter = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 };
