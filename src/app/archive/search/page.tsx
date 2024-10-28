d887faae49165e7527ff1b2d4cc2852c12b568ad
macjjuni <macjjuni@gmail.com>
2024-10-28 17:53:11 +0900

    feature. 검색 페이지 totalLength 및 페이징 처리 미완료

--- src/app/archive/search/page.tsx
+++ src/app/archive/search/page.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
 import Fallback from "@/app/archive/fallBack";
 import ArchiveSidebar from "@/layout/archiveSidebar/archiveSidebar";
-import ArchiveContent from "@/layout/archiveContainer/archiveContainer";
+import ArchiveContent from "@/layout/archiveContent/archiveContent";
 import type { Metadata } from "next";
 import { getMetadata } from "@/config/meta";
 import { getSearchArchive } from "@/utils/archive";
@@ -10,14 +10,14 @@ export async function generateMetadata({ searchParams }: { searchParams: { q: st
   return getMetadata(`검색: ${searchParams.q}`, null, `search?q=${searchParams.q}`, null);
 }
 
-export default async function ArchiveSearchPage({ searchParams }: { searchParams: { q: string } }) {
+export default async function ArchiveSearchPage({ searchParams }: { searchParams: { q: string; page: string; } }) {
   const searchKeyword = searchParams?.q || "";
-  const archives = await getSearchArchive(searchKeyword);
+  const { archives, totalLength } = await getSearchArchive(searchKeyword);
 
   return (
     <Suspense fallback={<Fallback />}>
       <ArchiveSidebar />
-      <ArchiveContent archives={archives} />
+      <ArchiveContent archives={archives} totalLength={totalLength} />
     </Suspense>
   );
 }
