9fcc670f43c2190f4ca29b64180ba5fbd039cb82
macjjuni <macjjuni@gmail.com>
2024-10-28 17:05:55 +0900

    feature. 프로필 컴포넌트 블로그 설정 파일에서 수정하도록 변경

--- src/component/sidebar/profile/profile.tsx
+++ src/component/sidebar/profile/profile.tsx
@@ -2,19 +2,15 @@ import Image from "next/image";
 import config from "@/config/config";
 import "./profile.scss";
 
-interface ProfileProps {
-  description?: string | null;
-}
+const { PROFILE_IMAGE, INTRO } = config?.profile || { PROFILE_IMAGE: null };
 
-const { PROFILE_IMAGE } = config?.profile || { PROFILE_IMAGE: null };
-
-export default function Profile({ description }: ProfileProps) {
+export default function Profile() {
   return (
     <div className="profile__card">
       {PROFILE_IMAGE && <Image className="profile__card__image" src={PROFILE_IMAGE} alt="profile image" width={360} height={360} />}
       <div className="profile__card__content">
         <p className="profile__card__content__name">꾸생</p>
-        <p className="profile__card__content__status-message">{description}</p>
+        <p className="profile__card__content__status-message">{INTRO}</p>
       </div>
     </div>
   );
