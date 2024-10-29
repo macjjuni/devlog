4b7ba2302c9fe0e0b6a31b20228e3bea98cc337f
macjjuni <macjjuni@gmail.com>
2024-10-29 11:01:57 +0900

    feature. 프로필 이미지 priority 속성 추가

--- src/component/sidebar/profile/profile.tsx
+++ src/component/sidebar/profile/profile.tsx
@@ -7,7 +7,7 @@ const { PROFILE_IMAGE, INTRO } = config?.profile || { PROFILE_IMAGE: null };
 export default function Profile() {
   return (
     <div className="profile__card">
-      {PROFILE_IMAGE && <Image className="profile__card__image" src={PROFILE_IMAGE} alt="profile image" width={360} height={360} />}
+      {PROFILE_IMAGE && <Image className="profile__card__image" src={PROFILE_IMAGE} alt="profile image" priority width={360} height={360} />}
       <div className="profile__card__content">
         <p className="profile__card__content__name">꾸생</p>
         <p className="profile__card__content__status-message">{INTRO}</p>
