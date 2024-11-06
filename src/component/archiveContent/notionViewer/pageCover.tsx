6d2a2b0cc524673628c4c1cf2539b9e924d9a5ca
macjjuni <macjjuni@gmail.com>
2024-11-06 21:33:13 +0900

    feature. 아카이브 상세 페이지 커버 이미지 중앙 위치로 고정

--- src/component/archiveContent/notionViewer/pageCover.tsx
+++ src/component/archiveContent/notionViewer/pageCover.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import "./pageCover.scss";
 
-const size = { width: 1200, height: 300 };
+const size = { width: 1052, height: 300 };
 
 export default function PageCover({ url, alt }: { url: string; alt: string }) {
   return (
