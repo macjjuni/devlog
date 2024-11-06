ac9031e2844cd2d1ab390e7bf66ce907934bacaf
macjjuni <macjjuni@gmail.com>
2024-11-06 14:53:21 +0900

    feature. kku-util 적용 #1
    - dateUtil 적용
    - 안쓰는 css 제거

--- src/component/archiveContent/archiveList/archiveList.tsx
+++ src/component/archiveContent/archiveList/archiveList.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import config from "@/config/notion.config";
 import usePageSize from "@/hook/usePageSize";
 import type { NotionPageProps } from "@/@types/notion";
-import date from "@/lib/date";
+import { calcCurrentDateDifference, getFormatDate } from "@/lib/date";
 import "./archiveList.scss";
 
 const { post } = config;
@@ -20,8 +20,7 @@ function ArchiveList({ pages }: { pages: NotionPageProps[] }) {
   // region [Privates]
 
   const pageList = useMemo(() => pages.slice(post.POSTS_PER_PAGE * (page - 1), post.POSTS_PER_PAGE * page), [page, pages]);
-
-  const isNew = useCallback((dateStr: string) => post.RECENT_DAY > date.nowDiff(dateStr), []);
+  const isNew = useCallback((dateStr: string) => (post.RECENT_DAY > calcCurrentDateDifference(dateStr, "day")), []);
 
   // endregion
 
@@ -33,7 +32,7 @@ function ArchiveList({ pages }: { pages: NotionPageProps[] }) {
             <h3 className="archive__list__item__title">{listItem.title}</h3>
             <div className="archive__list__item__right">
               <p>{listItem.category?.name}</p>
-              <p>{date.format(listItem.published)}</p>
+              <p>{getFormatDate(listItem.published)}</p>
             </div>
           </Link>
         </li>
--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,32 +1,10 @@
-import "dayjs/locale/ko";
-import dayjs from "dayjs";
+import { dateUtil } from "kku-util";
 
-dayjs.locale("ko");
+const { calcCurrentDateDifference, getFormatDate, getCurrentDate } = dateUtil;
 
-const defaultFormat = "YYYY.MM.DD";
-const defaultDetailFormat = "YYYY-MM-DD HH:mm";
-
-const now = dayjs();
-
-const date = {
-  now: () => now.format(defaultFormat),
-  nowDiff: (dateStr: string) => {
-    const nowDate = dayjs(new Date(), defaultFormat);
-    const diffTargetDate = dayjs(dateStr, defaultFormat);
-    return nowDate.diff(diffTargetDate, "d");
-  },
-  format: (dateStr: string | number) => dayjs(dateStr).format(defaultFormat),
-  formatDetail: (dateStr: string) => dayjs(dateStr).format(defaultDetailFormat),
-  diff: (date1: string, date2: string) => {
-    const formatDate1 = dayjs(date1, defaultDetailFormat);
-    const formatDate2 = dayjs(date2, defaultDetailFormat);
-    return formatDate1.diff(formatDate2, "second");
-  },
-};
-
-export function isTwoMinutesPassed(timestamp1: number, timestamp2: number): boolean {
-  const THREE_MINUTES_IN_MS = 2 * 60 * 1000; // 3분을 밀리초로 변환
+function isTwoMinutesPassed(timestamp1: number, timestamp2: number): boolean {
+  const THREE_MINUTES_IN_MS = 2 * 60 * 1000; // 2분을 밀리초로 변환
   return Math.abs(timestamp2 - timestamp1) >= THREE_MINUTES_IN_MS;
 }
 
-export default date;
+export { calcCurrentDateDifference, getFormatDate, isTwoMinutesPassed, getCurrentDate };
