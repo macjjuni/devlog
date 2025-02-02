bcb4b29debe877d7ebea007b0100a4de2fcb9274
macjjuni <macjjuni@gmail.com>
2025-02-02 21:29:35 +0900

    trivial. 푸터 텍스트 수정

--- src/layout/footer/footer.tsx
+++ src/layout/footer/footer.tsx
@@ -1,10 +1,13 @@
 import "./footer.scss";
 
 export default function Footer() {
+
   return (
     <footer className="footer">
       <div className="footer__wrapper">
-        <p className="footer__wrapper__text">{`${new Date().getFullYear()} kkussaeng.`}</p>
+        <p className="footer__wrapper__text">
+          {`${new Date().getFullYear()} ${process.env.NEXT_PUBLIC_LOGO?.toLocaleLowerCase()}.`}
+        </p>
       </div>
     </footer>
   );
